e7f71bac34d909dd0328fc943bca1c1de9822809
swatikanishu <nishuswatika@gmail.com>
2022-07-03 11:33:19 +0530

    Added: HashTable program is added

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -2,8 +2,26 @@ package com.bridgelabz;
 
 public class HashTableMain {
     public static void main(String[] args) {
-        System.out.println(" Welcome to hashtable program");
-    }
+        System.out.println("Welcome to hashtable Program");
+    MyHashTable<String, Integer> hashTable = new MyHashTable();
+    String sentence = "To be or not to be";
 
+    // Storing given sentence in an array.
+    // Converting the sentence to lower case and removing spaces.
+
+    String[] sentenceArray = sentence.toLowerCase().split(" ");
+
+    // Iterating over the array.
+        for (String word : sentenceArray) {
+        Integer value = hashTable.get(word);
+
+        if( value == null)
+            value = 1;
+        else
+            value = value + 1;
+        hashTable.add(word , value);
     }
+        System.out.println(hashTable);
+}
+}
 
